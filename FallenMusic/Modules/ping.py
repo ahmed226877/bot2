3958603e2551eeab79d8f617ac2c48fdb4e3fa78
ahmed226877 <ahmee3320@gmail.com>
2024-06-08 19:20:06 +0300

    Update ping.py

--- FallenMusic/Modules/ping.py
+++ FallenMusic/Modules/ping.py
@@ -60,7 +60,7 @@ async def ping_fallen(_, message: Message):
                     InlineKeyboardButton("الدعم", url=config.SUPPORT_CHAT),
                     InlineKeyboardButton(
                         "السوࢪس",
-                        url="https://t.me/gjllkc",
+                        url="https://t.me/B_M_W_1",
                     ),
                 ],
             ]
